37b8e9ab8749f39925de09b49a2245044063fa0f
chonticha-yasri <76036801+chonticha-yasri@users.noreply.github.com>
2020-12-21 19:00:16 +0700

    Add files via upload

---
+++ index/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box}
+body {font-family: Verdana, sans-serif; 
+  margin: 0;
+  padding: 0;
+  background: url(img8.png) no-repeat;
+  background-size: cover;}
+.mySlides {display: none}
+img {vertical-align: middle;}
+
+
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .prev, .next,.text {font-size: 11px}
+}
+</style>
+</head>
+   <br>
+   <br><br>
+  <center><img src="huajum2.png" width="500" height="400">
+    
+   <b> <p style= "font-size: 50px; color: azure;">WEB  <span style="color: rgb(39, 89, 155);">TECHNOLOGY</span></p></b>
+   <a  href="contact.html" style="font-size:25px;">Contect Us </a>
+  </center>
+  <div class="text"> </div>
+  
+</div>
+
+
+
+</body>
+</html> 
